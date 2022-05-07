d0a870d933534c3d5f90b36662c58ac1636d4112
Viktor <vityaschel@utidteam.com>
2022-05-08 00:54:14 +0400

    cards content

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,7 +3,6 @@ import styles from './styles.module.scss'
 import { Button } from '@mui/material'
 import React from 'react'
 import cx from 'classnames'
-import { theme } from '%/pages/_app'
 
 ButtonWithArrow.propTypes = {
   children: PropTypes.node
@@ -18,7 +17,7 @@ export default function ButtonWithArrow({ className, ...props }: ButtonWithArrow
       {props.children}
       <svg height="14" viewBox="0 0 25 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M24.0607 13.0607C24.6464 12.4749 24.6464 11.5251 24.0607 10.9393L14.5147 1.3934C13.9289 0.807613 12.9792 0.807613 12.3934 1.3934C11.8076 1.97919 11.8076 2.92893 12.3934 3.51472L20.8787 12L12.3934 20.4853C11.8076 21.0711 11.8076 22.0208 12.3934 22.6066C12.9792 23.1924 13.9289 23.1924 14.5147 22.6066L24.0607 13.0607ZM22 13.5L23 13.5L23 10.5L22 10.5L22 13.5Z" fill='currentColor' />
-        <line x1="22.5" y1="12" x2="1.5" y2="12" stroke={theme.palette.primary.main as string} strokeWidth="3" strokeLinecap="round" />
+        <line x1="22.5" y1="12" x2="1.5" y2="12" stroke='currentColor' strokeWidth="3" strokeLinecap="round" />
       </svg>
     </Button>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import Header from '%/components/Header'
 import type { NextPage } from 'next'
 import Head from 'next/head'
@@ -6,11 +7,13 @@ import styles from '../styles/Home.module.scss'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Typography from '@mui/material/Typography'
 import { useTranslation } from 'next-i18next'
-import ButtonWithArrow from '%/components/button'
+import ButtonWithArrow from '%/components/Button'
+import Button from '@mui/material/Button'
+import { Card } from '@mui/material'
+import { format } from 'date-fns'
+import { ru } from 'date-fns/locale'
 
 const Home: NextPage = () => {
-  const { t } = useTranslation()
-  
   return (
     <div className={styles.container}>
       <Head>
@@ -19,16 +22,97 @@ const Home: NextPage = () => {
       <Header />
 
       <main className={styles.main}>
-        <div className={styles.topBlock}>
-          <div className={styles.info}>
-            <Typography variant='h1' className={styles.heading}>{t('pages.home.heading')}</Typography>
-            <Typography variant='h2' className={styles.subheading}>{t('pages.home.subheading')}</Typography>
-            <ButtonWithArrow className={styles.button}>{t('pages.home.read_article')}</ButtonWithArrow>
-          </div>
-          <video muted autoPlay loop src='https://ik.imagekit.io/hloth/thinking_emoji.webm' />
+        <TopBlock />
+        <div className={styles.cards}>
+          <PageCard
+            translationCode='youtube'
+            img='https://ik.imagekit.io/hloth/the-archive-static/youtube.png?tr=n-landing_thumb'
+            lastUpdate={new Date('2022.04.20')}
+          />
+          <PageCard
+            translationCode='instagram'
+            img='https://ik.imagekit.io/hloth/the-archive-static/instagram.png?tr=n-landing_thumb'
+            lastUpdate={new Date('2022.05.08')}
+          />
+          <PageCard
+            translationCode='twitter'
+            img='https://ik.imagekit.io/hloth/the-archive-static/twitter.png?tr=n-landing_thumb'
+          />
+          <PageCard
+            translationCode='github'
+            img='https://ik.imagekit.io/hloth/the-archive-static/github.png?tr=n-landing_thumb'
+          />
         </div>
       </main>
     </div>
+  )
+}
+
+function TopBlock() {
+  const { t } = useTranslation()
+
+  return (
+    <div className={styles.topBlock}>
+      <div className={styles.info}>
+        <Typography variant='h1' className={styles.heading}>{t('pages.home.heading')}</Typography>
+        <Typography variant='h2' className={styles.subheading}>{t('pages.home.subheading')}</Typography>
+        <ButtonWithArrow className={styles.button} variant='contained'>
+          {t('pages.home.read_article')}
+        </ButtonWithArrow>
+      </div>
+      <div className={styles.video}>
+        <video muted autoPlay loop src='https://ik.imagekit.io/hloth/the-archive-static/thinking_emoji.webm' />
+      </div>
+    </div>
+  )
+}
+
+PageCard.propTypes = {
+  translationCode: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  lastUpdate: PropTypes.instanceOf(Date)
+}
+interface PageCardProps {
+  translationCode: string
+  img: string
+  lastUpdate?: Date
+}
+function PageCard(props: PageCardProps) {
+  const { t } = useTranslation()
+
+  return (
+    <Card className={styles.pageCard}>
+      <div className={styles.content}>
+        <div>
+          <Typography variant='h3' className={styles.title}>{t('pages.home.cards.' + props.translationCode + '.title')}</Typography>
+          <Typography variant='h4' className={styles.description}>{t('pages.home.cards.' + props.translationCode + '.description')}</Typography>
+          {props.lastUpdate && (
+            <Typography variant='caption' className={styles.lastUpdate}>{t('pages.home.cards.last_update')}: {
+              format(props.lastUpdate, 'd MMMM yyyy', { locale: ru })
+            }</Typography>
+          )}
+        </div>
+        {props.lastUpdate ? (
+          <ButtonWithArrow className={styles.button} variant='contained'>
+            {t('pages.home.cards.' + props.translationCode + '.button')}
+          </ButtonWithArrow>
+        ) : (
+          <Button className={styles.button} variant='contained' disabled>
+            {t('pages.home.cards.' + props.translationCode + '.button')}
+          </Button>
+        )}
+      </div>
+      <div className={styles.image}>
+        <Image
+          src={props.img}
+          alt={t('pages.home.cards.' + props.translationCode + '.image_alt')}
+          // height={200}
+          layout='fill'
+          objectFit='contain'
+          objectPosition='right'
+        />
+      </div>
+    </Card>
   )
 }
 
