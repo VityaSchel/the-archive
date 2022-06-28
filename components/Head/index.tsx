c475a58df1009c8d0a7cec3f6d14b31c8da47fec
Viktor <vityaschel@utidteam.com>
2022-06-28 22:12:00 +0400

    Remove favicon.svg

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -22,7 +22,6 @@ export default function CustomHead(props: HeadProps) {
       <meta name="description" content={props.description ?? t('head.description')} />
       <title>{props.title}</title>
       {props.children}
-      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
